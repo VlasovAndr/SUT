0fee4b217ef921c56b6fb42488f909dab3979339
Andrey Vlasov <lion32211@yandex.ru>
2024-02-25 20:08:56 +0100

    Update HomeController

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,37 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SUT.Models;
 using System.Diagnostics;
+using WebApp;
 
 namespace SUT.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        public IActionResult Demo()
-        {
-            return View();
-        }
+		public async Task<IActionResult> Product()
+		{
+			var productClient = new ProductAPI("https://localhost:7036", new HttpClient());
+			var result = await productClient.GetProductsAsync();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+			return View(result);
+		}
+
+		public IActionResult Demo()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
