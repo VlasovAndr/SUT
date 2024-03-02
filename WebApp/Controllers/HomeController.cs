5aa65d2d1efc7bd3e310e77dcf7661e805bb89fc
Andrey Vlasov <lion32211@yandex.ru>
2024-03-02 20:01:30 +0100

    Update namespaces in WebApp

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,46 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SUT.Models;
+using WebApp.Models;
 using System.Diagnostics;
-using WebApp;
 
-namespace SUT.Controllers
+namespace WebApp.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-		public async Task<IActionResult> Product()
-		{
-			var productClient = new ProductAPI("https://localhost:7036", new HttpClient());
-			var result = await productClient.GetProductsAsync();
+        public async Task<IActionResult> Product()
+        {
+            var productClient = new ProductAPI("https://localhost:7036", new HttpClient());
+            var result = await productClient.GetProductsAsync();
 
-			return View(result);
-		}
+            return View(result);
+        }
 
-		public IActionResult Demo()
-		{
-			return View();
-		}
+        public IActionResult Demo()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
--- WebApp/Models/ErrorViewModel.cs
+++ WebApp/Models/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-namespace SUT.Models
+namespace WebApp.Models
 {
     public class ErrorViewModel
     {
