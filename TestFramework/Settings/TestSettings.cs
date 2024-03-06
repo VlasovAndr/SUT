7917ecdb32a53ca9c9e0cffe1115c3114aacc97c
Andrey Vlasov <lion32211@yandex.ru>
2024-03-06 20:22:59 +0100

    Add config file

--- TestFramework/Settings/TestSettings.cs
+++ TestFramework/Settings/TestSettings.cs
@@ -5,4 +5,6 @@ namespace TestFramework.Settings;
 public class TestSettings
 {
     public BrowserType BrowserType { get; set; }
+    public Uri ApplicationUrl { get; set; }
+    public int TimeoutInterval { get; set; }
 }
