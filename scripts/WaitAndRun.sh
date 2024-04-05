c5d248a49316485d44a882d799737574c7df8a1c
Andrey Vlasov <lion32211@yandex.ru>
2024-04-05 17:59:04 +0200

    Added support for reporting artifacts in Github Action + Specflow LivingDoc

--- scripts/WaitAndRun.sh
+++ scripts/WaitAndRun.sh
@@ -14,3 +14,7 @@ until [ ]; do
 done 
 
 dotnet test --logger "console;verbosity=detailed"
+dotnet tool install --global SpecFlow.Plus.LivingDoc.CLI
+export PATH="$PATH:/root/.dotnet/tools"
+echo "Starting Specflow living doc report generation"
+livingdoc test-assembly "/src/TestProjectBDD/bin/Debug/net7.0/TestProjectBDD.dll" -t "/src/TestProjectBDD/bin/Debug/net7.0/TestExecution.json"
