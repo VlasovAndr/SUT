6fb62c410c789f3ee131987442e469d8881b2435
Andrey Vlasov <lion32211@yandex.ru>
2024-07-23 21:46:59 +0200

    Added AllureReporter

---
+++ AutomationTests/TestFramework/Reports/AllureReporter.cs
@@ -0,0 +1,26 @@
+﻿using Allure.Net.Commons;
+
+namespace TestFramework.Reports;
+
+public class AllureReporter : ITestReporter
+{
+    public void AddAttachment(string name, string type, byte[] attachment)
+    {
+        AllureApi.AddAttachment(name, type, attachment);
+    }
+
+    public void AddInfo(string message)
+    {
+        AllureApi.Step($"{message}");
+    }
+
+    public void AddParameter(string paramName, string paramValue)
+    {
+        AllureLifecycle.Instance.UpdateStep(stepResult =>
+        {
+            stepResult.parameters.Add(
+                new Parameter { name = paramName, value = paramValue }
+            );
+        });
+    }
+}
